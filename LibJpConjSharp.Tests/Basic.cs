2b8e940b1133dd1984fea416f90fc3e1bcc3a1c6
milleniumbug <milleniumbug@protonmail.com>
2020-09-27 20:23:04 +0200

    Add test cases

--- LibJpConjSharp.Tests/Basic.cs
+++ LibJpConjSharp.Tests/Basic.cs
@@ -11,7 +11,7 @@ namespace LibJpConjSharp.Tests
     public class Basic
     {
         [Test]
-        public void Awaseru()
+        public void Ichidan()
         {
             var word = "あわせる";
             var wordClass = EdictType.v1;
@@ -22,13 +22,149 @@ namespace LibJpConjSharp.Tests
         }
 
         [Test]
-        public void Suru()
+        [Ignore("archaic verbs not supported yet")]
+        public void NidanU()
         {
-            var word = "する";
-            var wordClass = EdictType.vs_i;
+            var word = "得";
+            var wordClass = EdictType.v2a_s;
+        }
 
-            Assert.AreEqual("|し|よう", JpConj.Conjugate(word, wordClass, CForm.Volitional, Politeness.Plain, Polarity.Affirmative));
-            Assert.AreEqual("|し|て", JpConj.Conjugate(word, wordClass, CForm.TeForm, Politeness.Plain, Polarity.Affirmative));
+        [Test]
+        [Ignore("archaic verbs not supported yet")]
+        public void YodanHuFu()
+        {
+            var word = "相語らふ";
+            var wordClass = EdictType.v4h;
+        }
+
+        [Test]
+        [Ignore("archaic verbs not supported yet")]
+        public void YodanRu()
+        {
+            var word = "離る";
+            var wordClass = EdictType.v4r;
+        }
+
+        [Test]
+        [Ignore("this one does not exist in JMdict so far, so we can't make a test case for now")]
+        public void GodanNotClassified()
+        {
+            var word = "";
+            var wordClass = EdictType.v5;
+        }
+
+        [Test]
+        public void GodanAru()
+        {
+            var word = "いらっしゃる";
+            var wordClass = EdictType.v5aru;
+        }
+
+        [Test]
+        public void GodanBu()
+        {
+            var word = "運ぶ";
+            var wordClass = EdictType.v5b;
+        }
+
+        [Test]
+        public void GodanGu()
+        {
+            var word = "游ぐ";
+            var wordClass = EdictType.v5g;
+        }
+
+        [Test]
+        public void GodanKu()
+        {
+            var word = "艶めく";
+            var wordClass = EdictType.v5k;
+        }
+
+        [Test]
+        public void GodanIkuYuku()
+        {
+            var word = "合点がいく"; // also 冴え行く
+            var wordClass = EdictType.v5k_s;
+        }
+
+        [Test]
+        public void GodanMu()
+        {
+            var word = "刷り込む";
+            var wordClass = EdictType.v5m;
+        }
+
+        [Test]
+        public void GodanNu()
+        {
+            var word = "死ぬ";
+            var wordClass = EdictType.v5n;
+        }
+
+        [Test]
+        public void GodanRu()
+        {
+            var word = "あやまる";
+            var wordClass = EdictType.v5r;
+        }
+
+        [Test]
+        public void GodanRuIrregular()
+        {
+            var word = "脚がある";
+            var wordClass = EdictType.v5r_i;
+        }
+
+        [Test]
+        public void GodanSu()
+        {
+            var word = "螫す";
+            var wordClass = EdictType.v5s;
+        }
+
+        [Test]
+        public void GodanTsu()
+        {
+            var word = "持つ";
+            var wordClass = EdictType.v5t;
+        }
+
+        [Test]
+        public void GodanU()
+        {
+            var word = "行う";
+            var wordClass = EdictType.v5u;
+        }
+
+        [Test]
+        public void GodanUSpecialClass()
+        {
+            var word = "信を問う";
+            var wordClass = EdictType.v5u_s;
+        }
+
+        [Test]
+        [Ignore("this one does not exist in JMdict so far, so we can't make a test case for now")]
+        public void GodanUru()
+        {
+            var word = "";
+            var wordClass = EdictType.v5uru;
+        }
+
+        [Test]
+        [Ignore("this one does not exist in JMdict so far, so we can't make a test case for now")]
+        public void GodanZu()
+        {
+            var word = "";
+            var wordClass = EdictType.v5z;
+        }
+
+        [Test]
+        public void IchidanZuru()
+        {
+            var word = "観ずる";
+            var wordClass = EdictType.vz;
         }
 
         [Test]
@@ -39,6 +175,44 @@ namespace LibJpConjSharp.Tests
 
             Assert.AreEqual("き||た", JpConj.Conjugate(word, wordClass, CForm.Past, Politeness.Plain, Polarity.Affirmative));
             Assert.AreEqual("き||て", JpConj.Conjugate(word, wordClass, CForm.TeForm, Politeness.Plain, Polarity.Affirmative));
+        }
+
+        [Test]
+        public void IrregularNu()
+        {
+            var word = "死ぬ";
+            var wordClass = EdictType.vn;
+        }
+
+        [Test]
+        public void NounSuru()
+        {
+            var word = "勉強";
+            var wordClass = EdictType.vs;
+        }
+
+        [Test]
+        public void Su()
+        {
+            var word = "熟す";
+            var wordClass = EdictType.vs_c;
+        }
+
+        [Test]
+        public void SuruIrregular()
+        {
+            var word = "する";
+            var wordClass = EdictType.vs_i;
+
+            Assert.AreEqual("|し|よう", JpConj.Conjugate(word, wordClass, CForm.Volitional, Politeness.Plain, Polarity.Affirmative));
+            Assert.AreEqual("|し|て", JpConj.Conjugate(word, wordClass, CForm.TeForm, Politeness.Plain, Polarity.Affirmative));
+        }
+
+        [Test]
+        public void SuruSpecial()
+        {
+            var word = "則する";
+            var wordClass = EdictType.vs_s;
         }
 
         [Test]
